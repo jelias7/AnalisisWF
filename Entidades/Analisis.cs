1516a381e6487dd6c20dc592caedc6742ba17dc7
Juan Rosario <juanelias18@hotmail.com>
2019-09-06 22:53:22 -0400

    Entidades

---
+++ Entidades/Analisis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Serializable]
+    public class Analisis
+    {
+        [Key]
+        public int AnalisisId { get; set; }
+        public string Paciente { get; set; }
+        public DateTime Fecha { get; set; }
+        public virtual List<AnalisisDetalle> Detalle {get;set;}
+
+        public Analisis()
+        {
+            AnalisisId = 0;
+            Paciente = string.Empty;
+            Fecha = DateTime.Now;
+            Detalle = new List<AnalisisDetalle>();
+        }
+    }
+}
---
+++ Entidades/AnalisisDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Serializable]
+    public class AnalisisDetalle
+    {
+        public int DetalleId { get; set; }
+        public string Analisis { get; set; }
+        public string Resultado { get; set; }
+        public AnalisisDetalle()
+        {
+            DetalleId = 0;
+            Analisis = string.Empty;
+            Resultado = string.Empty;
+        }
+        public AnalisisDetalle(int id, string analisis, string resultado)
+        {
+            DetalleId = id;
+            Analisis = analisis;
+            Resultado = resultado;
+        }
+    }
+}
